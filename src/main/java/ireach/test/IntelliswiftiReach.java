71b2f296e4b6846704c21affc63e6812266ce3c4
Hardik Bhatt <hardik.bhatt@intelliswift.com>
2021-04-28 12:19:35 +0530

    Second commit for iReach ClockIn Demo

--- src/main/java/ireach/test/IntelliswiftiReach.java
+++ src/main/java/ireach/test/IntelliswiftiReach.java
@@ -1,7 +1,5 @@
 package ireach.test;
 
-import java.util.Date;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -13,13 +11,11 @@ import org.testng.annotations.Test;
 public class IntelliswiftiReach {
 	
 	public static WebDriver driver;
-	public static int i = 1;
-	public static int j = 0;
 	
 	@Test
-	public void alaunch() throws Exception
+	public void clockInOut() throws Exception
 	{
-		System.setProperty("webdriver.chrome.driver", "D:/Intelliswift/Drivers/chromedriver_win32/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://ireach.darwinbox.in/user/login");
 		driver.manage().window().maximize();
@@ -27,17 +23,35 @@ public class IntelliswiftiReach {
 		driver.findElement(By.id("UserLogin_username")).sendKeys("hardik.bhatt@intelliswift.com");
 		driver.findElement(By.id("UserLogin_password")).sendKeys("hkbhkb@11");
 		driver.findElement(By.id("login-submit")).click();
+		
 		Thread.sleep(5000);
+		
+		if(driver.findElement(By.xpath("//*[@id=\"5\"]")).isDisplayed())
+		{
+			driver.findElement(By.xpath("//*[@id=\"5\"]")).click();
+			
+			if(driver.findElement(By.xpath("//*[@id=\"plus-status-btn\"]")).isDisplayed())
+			{
+				driver.findElement(By.xpath("//*[@id=\"plus-status-btn\"]")).click();
+			}
+			
+		}
+				
+		
 		WebDriverWait wait = new WebDriverWait(driver, 5);
 		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[@id=\"dasboard-bigheader\"]/header/div[4]/ul/li[1]/span/a"))));
 		driver.findElement(By.xpath("//*[@id=\"dasboard-bigheader\"]/header/div[4]/ul/li[1]/span/a")).click();
 		
+		Thread.sleep(5000);
+		
 		wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[@id=\"clokInClockout\"]/div/div/div[2]/div[3]/button"))));
-		//driver.findElement(By.xpath("//*[@id=\\\"clokInClockout\\\"]/div/div/div[2]/div[3]/button")).click();
+		//driver.findElement(By.xpath("//*[@id=\"clokInClockout\"]/div/div/div[2]/div[3]/button")).click();
+		
+		Thread.sleep(5000);
 		
 		String clockTime = driver.findElement(By.xpath("//*[@id=\"dasboard-bigheader\"]/header/div[4]/ul/li[1]/div/span[2]")).getText();
-		Date date = new Date();
 		System.out.println("Date: "+java.time.LocalDate.now()+" Time : "+clockTime);
+		
 	}
 	
 }
